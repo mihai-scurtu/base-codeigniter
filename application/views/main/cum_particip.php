6e8d84be6024ad955de247e58c7233ab09a5ac16
Mihai Scurtu <mscurtu90@gmail.com>
2014-08-29 14:54:15 +0300

    Remove existing templates

--- application/views/main/cum_particip.php
+++
@@ -1,6 +0,0 @@
-<div id="cum-particip" class="main-wrapper">
-	<div class="container">
-		<a href="https://www.facebook.com/Poiana" title"Poiana Facebook" target="_blank" class="fb"></a>
-		<a href="<?=url('castigatori')?>" title"Castigatori" class="castigatori"></a>
-	</div>
-</div>
